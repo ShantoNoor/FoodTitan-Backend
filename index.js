7f3f190949d3b85d99a93bb7f6c6da08a9f29234
Shanto Noor <shantonoor900@gmail.com>
2023-12-02 20:37:23 +0600

    signup/login route added

--- index.js
+++ index.js
@@ -23,6 +23,32 @@ app.get("/", async (req, res) => {
   return res.send("FoodTitan server is Running");
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    return res.send(await User.find(req.query));
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.message);
+    } else {
+      return res.status(500).send("Something went wrong");
+    }
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const result = await user.save();
+    return res.status(201).send(result);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.message);
+    } else {
+      return res.status(409).send("User already exists");
+    }
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
