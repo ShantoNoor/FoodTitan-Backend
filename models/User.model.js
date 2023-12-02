193ec295a3db105733882f3c2497380e12a698a0
Shanto Noor <shantonoor900@gmail.com>
2023-12-02 15:16:30 +0600

    first commit

---
+++ models/User.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    require: true,
+    type: String,
+  },
+  email: {
+    require: true,
+    type: String,
+    unique: true,
+  },
+  photo: {
+    require: false,
+    type: String,
+    default: ''
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
